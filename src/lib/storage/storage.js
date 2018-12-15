24b97029775f804d5da70c93b990d776b9d3b52d
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2018-12-14 17:08:21 -0800

    fixed the db.json issues and added the tasks completed in readme

--- src/lib/storage/storage.js
+++ src/lib/storage/storage.js
@@ -1,15 +1,11 @@
 'use strict';
 
 import memoryStorage from './memory.js';
-import fileStorage from './filesystem.js';
 import mongoStorage from './mongo.js';
 
 let dataStorageModule = {};
 
 switch( process.env.STORAGE ) {
-  case 'filesystem':
-    dataStorageModule = fileStorage;
-    break;
   case 'mongo':
     dataStorageModule = mongoStorage;
     break;
